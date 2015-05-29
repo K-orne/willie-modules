b3870ba484f385ea34690d73c71e19502f9a1ed3
khorne <r.overath@gmail.com>
2015-05-29 14:43:20 +0200

    added configure section

--- league.py
+++ league.py
@@ -3,12 +3,28 @@ import datetime
 from riotwatcher.riotwatcher import RiotWatcher
 from riotwatcher.riotwatcher import EUROPE_WEST
 
-API_KEY = ''
-w = RiotWatcher(API_KEY, default_region=EUROPE_WEST)
+
+def checkConfig(bot):
+    if not bot.config.has_option('league', 'api_key') or not bot.config.has_option('league', 'region'):
+        return False
+    else:
+        return [bot.config.league.api_key, bot.config.league.region]
+
+
+def configure(config):
+    """
+    | [league] | example     | purpose                                                   |
+    | api_key  | 123-456-789 | Grants access to the Riot API                             |
+    | region   | EUROPE_WEST | Preselects a region for which to access the API - defunct |
+    """
+    if config.option('Configure league.py', False):
+        config.interactive_add('league', 'api_key', "Riot API-Key")
+        config.interactive_add('league', 'region', "Default Region - Check riotwatcher")
 
 
 @willie.module.commands('lstatus')
 def lstatus(bot, trigger):
+    w = RiotWatcher(bot.config.league.api_key, default_region=EUROPE_WEST)
     status = w.get_server_status('euw')
     services = status.get('services')
     result = ""
@@ -16,12 +32,15 @@ def lstatus(bot, trigger):
     for s in services:
         seq.append(s.get('name') + ": " + (s.get('status')))
     result = ' - '.join(seq)
-    bot.say('League Status -> '+result)
+    bot.say('League Status -> ' + result + "Config-options: " + bot.config.league.api_key + ", " + bot.config.league.region)
 
 
 @willie.module.commands('sinfo')
 def sinfo(bot, trigger):
     name = trigger.group(2)
+    if bot.config.league.region == "EUROPE_WEST":
+        region = EUROPE_WEST
+    w = RiotWatcher(bot.config.league.api_key, default_region=region)
     summoner = w.get_summoner(name)
     result = "Summoner " + summoner['name'] + " is Level " + str(summoner['summonerLevel'])
     bot.say(result)
@@ -29,6 +48,7 @@ def sinfo(bot, trigger):
 
 @willie.module.commands('sid')
 def sid(bot, trigger):
+    w = RiotWatcher(bot.config.league.api_key, default_region=EUROPE_WEST)
     name = w.get_summoner(trigger.group(2))
     result = "Summoner ID for " + name['name'] + " is: " + str(name['id'])
     bot.say(result)
@@ -36,6 +56,7 @@ def sid(bot, trigger):
 
 @willie.module.commands('wotd')
 def wotd(bot, trigger):
+    w = RiotWatcher(bot.config.league.api_key, default_region=EUROPE_WEST)
     name = w.get_summoner(trigger.group(2))
     match = w.get_recent_games(name['id'])
     matches = match['games']
@@ -55,4 +76,4 @@ def wotd(bot, trigger):
     if delta > timer:
         bot.say("Last won match for " + name['name'] + " ended on: " + last_won_match.ctime() + ". Win of the day is available.")
     else:
-        bot.say("Last won match for " + name['name'] + " ended on: " + last_won_match.ctime() + ". Win of the day is available at " + (last_won_match + timer).strftime("%H:%M:%S"))
+        bot.say("Last won match for " + name['name'] + " ended on: " + last_won_match.ctime() + ". Win of the day is available at " + (last_won_match + timer).strftime("%d.%m - %H:%M:%S"))
